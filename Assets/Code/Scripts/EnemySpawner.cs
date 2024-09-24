2dd2dd5bc12a4e1ab3b56f1461d69b26c6765e8d
MightBeCrit <ayush.patel@my.rongotai.school.nz>
2024-09-25 09:45:38 +1200

    Update EnemySpawner.cs
    eps and randomly generated enemy spawns

--- Assets/Code/Scripts/EnemySpawner.cs
+++ Assets/Code/Scripts/EnemySpawner.cs
@@ -13,6 +13,7 @@ public class EnemySpawner : MonoBehaviour {
     [SerializeField] private float enemiesPerSecond = 0.5f;
     [SerializeField] private float timeBetweenWaves = 5f;
     [SerializeField] private float difficultyScalingFactor = 0.75f;
+    [SerializeField] private float enemiesPerSecondCap = 15f;
 
     [Header("Events")]
     public static UnityEvent onEnemyDestroy = new UnityEvent();
@@ -20,7 +21,8 @@ public class EnemySpawner : MonoBehaviour {
     private int currentWave = 1;
     private float timeSinceLastSpawn;
     private int enemiesAlive;
-    private int enemiesLeftToSpawn;
+    private int enemiesLeftToSpawn; 
+    private float eps; // enemies per second
     private bool isSpawning = false;
 
     private void Awake() {
@@ -36,7 +38,7 @@ public class EnemySpawner : MonoBehaviour {
 
         timeSinceLastSpawn += Time.deltaTime;
 
-        if (timeSinceLastSpawn >= (1f/enemiesPerSecond) && enemiesLeftToSpawn > 0) {
+        if (timeSinceLastSpawn >= (1f / eps) && enemiesLeftToSpawn > 0) {
             SpawnEnemy();
             enemiesLeftToSpawn--;
             enemiesAlive++;
@@ -56,17 +58,19 @@ public class EnemySpawner : MonoBehaviour {
         yield return new WaitForSeconds(timeBetweenWaves);
         isSpawning = true;
         enemiesLeftToSpawn = EnemiesPerWave();
+        eps = EnemiesPerSecond();
     }
 
     private void EndWave() {
         isSpawning = false;
         timeSinceLastSpawn = 0f;
-        StartCoroutine(StartWave());
         currentWave++;
+        StartCoroutine(StartWave());
     }
 
     private void SpawnEnemy() {
-        GameObject prefabToSpawn = enemyPrefabs[0];
+        int index = Random.Range(0, enemyPrefabs.Length);
+        GameObject prefabToSpawn = enemyPrefabs[index];
         Instantiate(prefabToSpawn, LevelManager.main.startPoint.position, Quaternion.identity);
     }
 
@@ -74,4 +78,7 @@ public class EnemySpawner : MonoBehaviour {
         return Mathf.RoundToInt(baseEnemies * Mathf.Pow(currentWave, difficultyScalingFactor));
     }
 
+        private float EnemiesPerSecond() {
+        return Mathf.Clamp(enemiesPerSecond * Mathf.Pow(currentWave, difficultyScalingFactor), 0f, enemiesPerSecondCap);
+    }
 }
